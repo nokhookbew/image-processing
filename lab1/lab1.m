037f18dba70f882042f9250d67acbce83137138d
Bewvy Worapat <nokhook_bew@hotmail.com>
2020-01-06 13:10:24 +0700

    modified lab1 code

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -1,19 +1,21 @@
 image = imread('test_image.tif');
-% r = image(:,:,1);
-% figure(1)
-% imshow(r);
-% g = image(:,:,2);
-% figure(2)
-% imshow(g);
-% b = image(:,:,3);
-% figure(3)
-% imshow(b);
+figure(6);
+imshow(image);
+r = image(:,:,1);
+figure(1);
+imshow(r);
+g = image(:,:,2);
+figure(2);
+imshow(g);
+b = image(:,:,3);
+figure(3);
+imshow(b);
 
 
-res1 = bitshift(image, 3);
-figure(4)
-imshow(res1);
+res1 = bitsll(image,3);
+figure(4);
+imshow(res);
 
-res2 = bitshift(image, 7);
-figure(5)
-imshow(res2)
+res2 = bitsll(image, 7);
+figure(5);
+imshow(res2);
