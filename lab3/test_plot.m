a8914e0e65bd800684aaf421f8022710b1e38002
Bewvy Worapat <nokhook_bew@hotmail.com>
2020-01-13 13:39:34 +0700

    add plot graph

--- lab3/test_plot.m
+++ lab3/test_plot.m
@@ -4,6 +4,9 @@ subplot(1,2,1);
 imshow(mri_img);
 subplot(1,2,2);
 imhist(mri_img,64);
+[J,T] = histeq(mri_img);
+figure(4);
+plot((0:255)/255,T);
 
 mri_img_adj = imadjust(mri_img, stretchlim(mri_img), []); 
 figure(2);
@@ -11,6 +14,9 @@ subplot(1,2,1);
 imshow(mri_img_adj);
 subplot(1,2,2);
 imhist(mri_img_adj,64);
+[K,T] = histeq(mri_img_adj);
+figure(5);
+plot((0:255)/255,T);
 
 mri_img_his = histeq(mri_img);
 figure(3);
@@ -18,3 +24,6 @@ subplot(1,2,1);
 imshow(mri_img_his);
 subplot(1,2,2);
 imhist(mri_img_his,64);
+[L,T] = histeq(mri_img_his);
+figure(6);
+plot((0:255)/255,T);
