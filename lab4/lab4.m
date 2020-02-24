e23dbeee0398b008ab4247183992237a7c671cf3
Bewvy Worapat <nokhook_bew@hotmail.com>
2020-02-24 11:02:55 +0700

    add commect to unuse code

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,43 +1,46 @@
 img = imread('honeypot.jpg');
-imgd = double(img);
+img_d = double(img);
 figure(1);
 imshow(img);
 
-h = fspecial('average', 33);
-avg = imfilter(imgd, h);
+h = fspecial('average', 40);
+
+avg = imfilter(img_d, h);
+figure(99);
+imshow(uint8(avg));
 
 h_x = fspecial('sobel');
 
 h_y = h_x';
 
-f_img_x = imfilter(imgd, h_x);
+f_img_x = imfilter(img_d, h_x);
 figure(2);
 imshow(f_img_x);
 
-f_img_y = imfilter(imgd, h_y);
+f_img_y = imfilter(img_d, h_y);
 figure(3);
 imshow(f_img_y);
 
-[row, col, ~] = size(imgd);
+% [row, col, ~] = size(imgd);
 
-new_img = ones(row, col);
+% new_img = ones(row, col);
 
 % for i = 1 :row
 %    for j = 1 :col
 %        new_img(i,j) = sqrt((f_img_x(i, j).^2) + (f_img_y(i, j).^2));
 %     end
 % end
-new_img = sqrt((f_img_x.^2) + (f_img_y.^2));
+f_img = sqrt((f_img_x.^2) + (f_img_y.^2));
 
-new_img_u = uint8(new_img);
+f_img_u = uint8(f_img);
 figure(4);
-imshow(new_img_u);
+imshow(f_img_u);
 
 avg_u = uint8(avg);
 
-avg_u = avg_u-new_img_u;
+result = avg_u-f_img_u;
 figure(5);
-imshow(avg_u);
+imshow(result);
 
 
 
